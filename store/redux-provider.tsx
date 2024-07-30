c513b2099a29ad772121840170e0b1edf7fb3ad5
saketh <saketh.reddy@trainingmug.com>
2024-07-31 02:12:29 +0530

    basic ui completed

---
+++ store/redux-provider.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import store from '@/store/store';
+import React, { PropsWithChildren } from 'react';
+import { Provider } from 'react-redux';
+
+
+export default function Providers({ children }: PropsWithChildren) {
+  return (
+    <Provider store={store}>
+      
+        {children}
+     
+    </Provider>
+  );
+}
